fc5ab3b8870e0b3fae2e7e617f987075dd01ac32
D.N.Singh <76054685+DNSingh-15@users.noreply.github.com>
2024-03-26 01:15:59 +0530

    Update HeaderSocials.tsx

--- components/header/HeaderSocials.tsx
+++ components/header/HeaderSocials.tsx
@@ -4,10 +4,10 @@ import { TfiEmail } from 'react-icons/tfi';
 
 const HeaderSocials = () => {
     return (
-        <div className='flex flex-col items-center gap-2 absolute left-0 bottom-12'>
-            <a href='www.linkedin.com' target="_blank" className='text-primary'><BsLinkedin /></a>
-            <a href='www.github.com' target="_blank" className='text-primary'><BsGithub /></a>
-            <a href='mailto:youremail@example.com' className='text-primary'><TfiEmail /></a>
+        <div className='flex flex-col items-center gap-6 absolute left-0 bottom-12'>
+            <a href="https://www.linkedin.com/in/%F0%9D%90%83-%F0%9D%90%8D-%F0%9D%90%92%F0%9D%90%88%F0%9D%90%8D%F0%9D%90%86%F0%9D%90%87-49b85b1b2/" target="_blank" className='text-primary text-[14px]'><BsLinkedin /></a>
+            <a href='https://github.com/DNSingh-15' target="_blank" className='text-primary text-[14px]'><BsGithub /></a>
+            <a href="mailto:asmrdnsingh@gmail.com" target="_blank" className='text-primary text-[14px]'><TfiEmail /></a>
         </div>
     );
 }
